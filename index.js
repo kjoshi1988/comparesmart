1acfa94bcd4f3886b9489e0261d3d6194bb62dcd
Kapil <kapil.joshi@nextag.com>
2016-01-26 18:02:39 +0530

    index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 var server = http.createServer(function(req,resp){
    resp.writeHead(200, {'content-type':'text/html'});
    resp.end('Hello world');
-}).listen();
+}).listen(8080);
 
 console.log("server started at %s", server.address().port);
